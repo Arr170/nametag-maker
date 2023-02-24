96a350d3b20deea8e8e234a0628aa30124fff767
Arr170 <kuprin.ars@gmail.com>
2023-02-24 20:41:02 +0100

    something started to work

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [data, setData] = useState()
+  const [file, setFiles] = useState()
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  async function testFunc () {
+    const result = await axios.get('/test')
+    console.log(result.data.one)
+    setData(result.data.one)
+  }
+  function handleChange (event){
+    setFiles(event.target.files[0])
+    console.log(event.target.files[0])
+  }
+  async function doupload (event) {
+    event.preventDefault()
+    
+    //const entry = await document.getElementById("image-file").files[0]
+    
+    const formData = new FormData()
+    
+    //formData.append('file', file)
+    formData.append('fuck it', 'fuck it')
+    formData.append('file', file)
+
+    console.log('to upload', formData.type)
+    axios.post('/data', formData)
+    .then(res => console.log(res))
+    
+  }
+  //console.log(data)
+  return(
+    <div>
+        <p>Hello</p>
+        <button onClick = {testFunc}>click</button>
+        <p>{data}</p>
+        <input id="image-file" type="file" name = "file" onChange={handleChange}/>
+        <button type = 'submit' onClick = {doupload}>upload</button>
+
     </div>
-  );
+  )
 }
 
 export default App;
+//{data.one.map( num => <p key = {num}>{num}</p>)}
