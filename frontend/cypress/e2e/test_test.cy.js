47fd06bc6050c9e2812b229b9f66b5036233be4a
Arr170 <kuprin.ars@gmail.com>
2023-04-14 10:49:31 +0200

    m

--- frontend/cypress/e2e/test_test.cy.js
+++
@@ -1,9 +0,0 @@
-describe('testing test', () => {
-  it('tries to work', () => {
-    cy.visit('https://nametags.onrender.com')
-
-    cy.contains('small').click()
-
-    cy.get('.inputs').type('fuckoff')
-  })
-})
